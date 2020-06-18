fc22137bc4dc5eac7cecc9ec0b53c119033a2be6
Akash Magnadia <akash.magnadia@gmail.com>
2020-06-17 23:19:10 -0500

    Created CMakeList file to run Wumpus.cpp

---
+++ cmake-build-debug/CMakeFiles/simple_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_example.dir/Wumpus.cpp.obj"
+  "simple_example.exe"
+  "simple_example.exe.manifest"
+  "simple_example.lib"
+  "simple_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
